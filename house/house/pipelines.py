83894993ef736346c85de6adab884a6238e7bbdf
zongfangfang <1047670763@qq.com>
2018-10-09 23:00:22 +0800

    add house

---
+++ house/house/pipelines.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.exporters import JsonItemExporter
+
+class HousePipeline(object):
+    def __init__(self):
+        self.file = open('house.json','wb')
+        self.export = JsonItemExporter(self.file,encoding ='utf-8')
+        self.export.start_exporting()
+    def close_spider(self,spider):
+        self.export.finish_exporting()
+        self.file.close()
+    def process_item(self, item, spider):
+        if item['price']:
+            self.export.export_item(item)
+            self.file.write('\n')
+        return item
