5efaa8863ceb669e8cd861f53de9ff0190b67589
zongff <zongff@tiansu-china.com>
2018-12-29 17:25:21 +0800

    新增页面 删选出半年内的

--- house/model/mymssql/generate_data2.py
+++ house/model/mymssql/generate_data2.py
@@ -9,11 +9,13 @@ rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
 
 def gener():
-    print '写入数据库'
+    print u'写入数据库'
     ms_sql = MsSql()
     cur = ms_sql.conn
     # cur.execute("SELECT * from info WHERE price!='待定' order by price asc")
-    cur.execute('SELECT * from department  ORDER BY substring_index(price,"元",1)+0 asc')
+    # cur.execute('SELECT * from department  ORDER BY substring_index(price,"元",1)+0 asc')
+    # cur.execute('SELECT * from department  where substring_index(price,"元",1)+0 < 30000 ORDER BY substring_index(price,"元",1)+0 asc')
+    cur.execute('SELECT * from department  where substring_index(price,"元",1)+0 < 30000 ORDER BY cdate desc')
     ms_sql.ms.close()
     try:
         with open(rootPath+r'\mymssql\statistic_data1.json','w+') as f:
