a87e6a43c638fba6f47dc15882e9e4d885e2ff98
zongfangfang <1047670763@qq.com>
2021-05-07 22:43:13 +0800

    car

---
+++ carReports/carReports/main.py
@@ -0,0 +1,17 @@
+# coding:utf-8
+
+from scrapy.cmdline import execute
+import os
+import sys
+# 添加当前项目的绝对地址
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
+
+
+
+
+
+# 执行 scrapy 内置的函数方法execute，使用 crawl 爬取并调试，最后一个参数jobbole 是我的爬虫文件名
+execute(['scrapy', 'crawl', 'car'])
